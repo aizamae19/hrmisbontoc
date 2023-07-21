827d6ce10813544abbb4db7c90e44ccea14d833e
Aiza Genol <genolaiza@gmail.com>
2023-07-21 16:29:45 +0800

    added

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -9,40 +9,27 @@
                     <form method="POST" action="{{route('register.custom')}}">
                         @csrf
                         @include('layouts.partials.messages')
-                         <div class="row">
+                         <div class="row row-space">
                             <div class="col-2">
                                 <div class="input-group">
                                     <label class="label">Name</label>
                                     <input class="input--style-4" type="text" name="name">
                                 </div>
                             </div>
-<<<<<<< Updated upstream
-=======
-                        </div>
-                        <div class="row">
->>>>>>> Stashed changes
                             <div class="col-2">
                                 <div class="input-group">
                                     <label class="label">Email</label>
                                     <input class="input--style-4" type="text" name="email">
                                 </div>
                             </div>
-<<<<<<< Updated upstream
-=======
                         </div>
-                        <div class="row">
->>>>>>> Stashed changes
+                        <div class="row row-space">
                             <div class="col-2">
                                 <div class="input-group">
                                     <label class="label">Username</label>
                                     <input class="input--style-4" type="text" name="username">
                                 </div>
                             </div>
-<<<<<<< Updated upstream
-=======
-                        </div>
-                        <div class="row">
->>>>>>> Stashed changes
                             <div class="col-2">
                                 <div class="input-group">
                                     <label class="label">Password</label>
