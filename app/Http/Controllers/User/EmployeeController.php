12555ea156914abaca19655cb3ec102e6f6c4fa2
aizamae19 <genolaiza@gmail.com>
2023-11-19 10:56:03 +0700

    added by genol

--- app/Http/Controllers/User/EmployeeController.php
+++ app/Http/Controllers/User/EmployeeController.php
@@ -5,15 +5,14 @@ namespace App\Http\Controllers\User;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Employee;
-
+use Illuminate\Support\Facades\Auth;
 
 class EmployeeController extends Controller
 {
-    public function viewemployee(Request $request){
-        $employees=Employee::where('id',$request->id)->first();
+    public function viewemployee(){
+    $email = Auth::user()->email;
+    $employee = Employee::where('personalemail', $email)->first();
 
-        return view('users.myprofile.index',[
-                'employees'=>$employees
-        ]);
+    return view('users.myprofile.index', ['employee' => $employee]);
     }
 }
--- resources/views/users/myprofile/index.blade.php
+++ resources/views/users/myprofile/index.blade.php
@@ -1,16 +1,16 @@
 @extends('layouts.users')
 
 @section('content')
+@if(isset($employee))
     <section style="background-color: #eee; color:black;">
   <div class="container py-5"> 
-
     <div class="row" >
       <div class="col-lg-4">
         <div class="card mb-4">
           <div class="card-body text-center">
             <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-chat/ava3.webp" alt="avatar"
               class="rounded-circle img-fluid" style="width: 150px;">
-            <h5 class="my-3">John Smith</h5>
+            <h5 class="my-3">{{ $employee->firstname }} {{ $employee->middlename }} {{ $employee->lastname }} {{ $employee->suffix}}</h5>
             <div class="d-flex justify-content-center mb-2">
               <button type="button" class="btn btn-primary">Change Profile</button>
               <button type="button" class="btn btn-outline-primary ms-1">Edit</button>
@@ -27,7 +27,7 @@
                 <p class="mb-0">Full Name</p>
               </div>
               <div class="col-sm-9">
-                <p class="text-muted mb-0">Johnatan Smith</p>
+                <p class="text-muted mb-0">{{ $employee->firstname }} {{ $employee->middlename }} {{ $employee->lastname }} {{ $employee->suffix}}</p>
               </div>
             </div>
             <hr>
@@ -36,7 +36,7 @@
                 <p class="mb-0">Suffix</p>
               </div>
               <div class="col-sm-9">
-                <p class="text-muted mb-0">Jr.</p>
+                <p class="text-muted mb-0">{{ $employee->suffix }}</p>
               </div>
             </div>
             <hr>
@@ -45,7 +45,7 @@
                 <p class="mb-0">Address</p>
               </div>
               <div class="col-sm-9">
-                <p class="text-muted mb-0">(097) 234-5678</p>
+                <p class="text-muted mb-0">{{ $employee->address }}</p>
               </div>
             </div>
             <hr>
@@ -54,7 +54,7 @@
                 <p class="mb-0">Marital Status</p>
               </div>
               <div class="col-sm-9">
-                <p class="text-muted mb-0">(098) 765-4321</p>
+                <p class="text-muted mb-0">{{ $employee->maritalstatus }}</p>
               </div>
             </div>
           </div>
@@ -68,7 +68,7 @@
                 <p class="mb-0">Date of Birth</p>
               </div>
               <div class="col-sm-9">
-                <p class="text-muted mb-0">Bay Area, San Francisco, CA</p>
+                <p class="text-muted mb-0">{{ $employee->dateofbirth }}</p>
               </div>
             </div>
             <hr>
@@ -77,7 +77,7 @@
                 <p class="mb-0">Gender</p>
               </div>
               <div class="col-sm-9">
-                <p class="text-muted mb-0">Male</p>
+                <p class="text-muted mb-0">{{ $employee->gender }}</p>
               </div>
             </div>
             <hr>
@@ -86,7 +86,7 @@
                 <p class="mb-0">Blood Type</p>
               </div>
               <div class="col-sm-9">
-                <p class="text-muted mb-0">O+</p>
+                <p class="text-muted mb-0">{{ $employee->bloodtype }}</p>
               </div>
             </div>
             <hr>
@@ -95,7 +95,7 @@
                 <p class="mb-0">Contact Number</p>
               </div>
               <div class="col-sm-9">
-                <p class="text-muted mb-0">(097) 234-5678</p>
+                <p class="text-muted mb-0">{{ $employee->contactnumber }}</p>
               </div>
             </div>
             <hr>
@@ -104,7 +104,7 @@
                 <p class="mb-0">Person to Contact</p>
               </div>
               <div class="col-sm-9">
-                <p class="text-muted mb-0">(098) 765-4321</p>
+                <p class="text-muted mb-0">{{ $employee->persontocontact }}</p>
               </div>
             </div>
             <hr>
@@ -113,7 +113,7 @@
                 <p class="mb-0">Personal Email</p>
               </div>
               <div class="col-sm-9">
-                <p class="text-muted mb-0">jonathan@gmail.com</p>
+                <p class="text-muted mb-0">{{ $employee->personalemail }}</p>
               </div>
             </div>
             <hr>
@@ -122,7 +122,7 @@
                 <p class="mb-0">Corporate Email</p>
               </div>
               <div class="col-sm-9">
-                <p class="text-muted mb-0">jonathanhr@gmail.com</p>
+                <p class="text-muted mb-0">{{ $employee->corporateemail }}</p>
               </div>
             </div>
           </div>
@@ -138,7 +138,7 @@
                 <p class="mb-0">Course</p>
               </div>
               <div class="col-sm-9">
-                <p class="text-muted mb-0">Bachelor of Science in Information Technology</p>
+                <p class="text-muted mb-0">{{ $employee->course }}</p>
               </div>
             </div>
             <hr>
@@ -147,7 +147,7 @@
                 <p class="mb-0">Graduate Studies</p>
               </div>
               <div class="col-sm-9">
-                <p class="text-muted mb-0">Master of Science in Information Technology</p>
+                <p class="text-muted mb-0">{{ $employee->graduate }}</p>
               </div>
             </div>
             <hr>
@@ -156,7 +156,7 @@
                 <p class="mb-0">CSC Professional Eligibility</p>
               </div>
               <div class="col-sm-9">
-                <p class="text-muted mb-0">Career Service Professional</p>
+                <p class="text-muted mb-0">{{ $employee->csc }}</p>
               </div>
             </div>
             <hr>
@@ -165,7 +165,7 @@
                 <p class="mb-0">Date of Issuance PRC ID</p>
               </div>
               <div class="col-sm-9">
-                <p class="text-muted mb-0">06-19-2023</p>
+                <p class="text-muted mb-0">{{ $employee->dateofissuance }}</p>
               </div>
             </div>
             <hr>
@@ -174,7 +174,7 @@
                 <p class="mb-0">Date of Validity PRC ID</p>
               </div>
               <div class="col-sm-9">
-                <p class="text-muted mb-0">06-19-2025</p>
+                <p class="text-muted mb-0">{{ $employee->dateofvalidity }}</p>
               </div>
             </div>
           </div>
@@ -190,7 +190,7 @@
                 <p class="mb-0">Personnel ID</p>
               </div>
               <div class="col-sm-9">
-                <p class="text-muted mb-0">Bachelor of Science in Information Technology</p>
+                <p class="text-muted mb-0">{{ $employee->personnel }}</p>
               </div>
             </div>
             <hr>
@@ -199,7 +199,7 @@
                 <p class="mb-0">Status</p>
               </div>
               <div class="col-sm-9">
-                <p class="text-muted mb-0">Master of Science in Information Technology</p>
+                <p class="text-muted mb-0">{{ $employee->status }}</p>
               </div>
             </div>
             <hr>
@@ -208,7 +208,7 @@
                 <p class="mb-0">Department</p>
               </div>
               <div class="col-sm-9">
-                <p class="text-muted mb-0">Career Service Professional</p>
+                <p class="text-muted mb-0">{{ $employee->department }}</p>
               </div>
             </div>
             <hr>
@@ -217,7 +217,7 @@
                 <p class="mb-0">Position Title</p>
               </div>
               <div class="col-sm-9">
-                <p class="text-muted mb-0">06-19-2023</p>
+                <p class="text-muted mb-0">{{ $employee->position }}</p>
               </div>
             </div>
             <hr>
@@ -226,7 +226,7 @@
                 <p class="mb-0">Date of Joining</p>
               </div>
               <div class="col-sm-9">
-                <p class="text-muted mb-0">06-19-2025</p>
+                <p class="text-muted mb-0">{{ $employee->dateofjoining }}</p>
               </div>
             </div>
             <hr>
@@ -235,7 +235,7 @@
                 <p class="mb-0">Date of Mandatory Retirement</p>
               </div>
               <div class="col-sm-9">
-                <p class="text-muted mb-0">06-19-2025</p>
+                <p class="text-muted mb-0">{{ $employee->dateofleaving }}</p>
               </div>
             </div>
             <hr>
@@ -244,7 +244,7 @@
                 <p class="mb-0">Salary Grade</p>
               </div>
               <div class="col-sm-9">
-                <p class="text-muted mb-0">06-19-2025</p>
+                <p class="text-muted mb-0">{{ $employee->salary }}</p>
               </div>
             </div>
             <hr>
@@ -253,7 +253,7 @@
                 <p class="mb-0">Step</p>
               </div>
               <div class="col-sm-9">
-                <p class="text-muted mb-0">06-19-2025</p>
+                <p class="text-muted mb-0">{{ $employee->step }}</p>
               </div>
             </div>
           </div>
@@ -265,5 +265,5 @@
     </div>
   </div>
 </section>
-
+@endif
 @endsection
