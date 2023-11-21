ba667b4c62d73bc4fc4b66952be3e24925052685
aizamae19 <genolaiza@gmail.com>
2023-11-21 12:27:34 +0700

    added by genol

--- resources/views/admin/dashboard/index.blade.php
+++ resources/views/admin/dashboard/index.blade.php
@@ -13,104 +13,204 @@
                     </ol>
                 </div>
             </div>
-            <!-- Container fluid  -->
-            <div class="container-fluid">
-                <!-- Row -->
-                <div class="row">
-                    <!-- Column -->
-                    <div class="col-lg-4 col-md-6">
-                        <div class="card">
-                            <div class="card-body">
-                                <div class="d-flex flex-row">
-                                    <div class="round align-self-center round-primary"><i class="ti-user"></i></div>
-                                    <div class="m-l-10 align-self-center">
-                                    @if($noemployees=App\Models\Employee::count())
-                                        <h3 class="m-b-0">
-                                            {{$noemployees}}
-                                        </h3>
-                                    @endif
-                                        <a href="{{ route('employee.employees')}}" class="text-muted m-b-0">View Details</a></div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <!-- Column -->
-                    <div class="col-lg-4 col-md-6">
-                        <div class="card">
-                            <div class="card-body">
-                                <div class="d-flex flex-row">
-                                    <div class="round align-self-center round-info"><i class="ti-file"></i></div>
-                                    <div class="m-l-10 align-self-center">
-                                        @if($noleaveapplications=App\Models\Leaveapplication::count())
-                                        <h3 class="m-b-0">
-                                            {{$noleaveapplications}}
-                                        </h3>
-                                    @endif
-                                        <a href="{{ route('leaveapplication')}}" class="text-muted m-b-0">View Details</a>
-                                        </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <!-- Column -->
-                    <div class="col-lg-4 col-md-6">
-                        <div class="card">
-                            <div class="card-body">
-                                <div class="d-flex flex-row">
-                                    <div class="round align-self-center round-danger"><i class="ti-user"></i></div>
-                                    <div class="m-l-10 align-self-center">
-                                        <h3 class="m-b-0">
-                                            
-                                        </h3>
-                                        <a href="" class="text-muted m-b-0">View Details</a></div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    
-                </div>
-                <!-- Row -->
-                <!-- Row -->
-                
-                <div class="row ">
-                    <!-- Column -->
-                    <div class="col-md-6 col-lg-4 col-xlg-3">
-                        <div class="card card-inverse card-info">
-                            <div class="box bg-primary text-center">
-                                @if($noemployees=App\Models\Employee::count())
-                                <h1 class="font-light text-white">
-                                    {{$noemployees}}
-                                </h1>
-                                @endif
-                                <h6 class="text-white">Former Employees</h6>
-                            </div>
-                        </div>
-                    </div>
+<div class="container bootstrap snippets bootdey">
+  <div class="row">
+    <div class="col-lg-4 col-sm-8">
+      <div class="circle-tile ">
+        <a href="#"><div class="circle-tile-heading dark-blue"><i class="fa fa-users fa-fw fa-3x"></i></div></a>
+        <div class="circle-tile-content dark-blue">
+          <div class="circle-tile-description text-faded"> Former Employees</div>
+            @if($noemployees=App\Models\Employee::count())
+                <div class="circle-tile-number text-faded ">{{$noemployees}}</div> 
+            @endif
+          <a class="circle-tile-footer" href="{{ route('employee.employees')}}">More Info<i class="fa fa-chevron-circle-right"></i></a>
+        </div>
+      </div>
+    </div>
+     
+    <div class="col-lg-4 col-sm-8">
+      <div class="circle-tile ">
+        <a href="#"><div class="circle-tile-heading red"><i class="fa fa-users fa-fw fa-3x"></i></div></a>
+        <div class="circle-tile-content red">
+          <div class="circle-tile-description text-faded"> Leave Application </div>
+            @if($noleaveapplications=App\Models\Leaveapplication::count())
+                <div class="circle-tile-number text-faded ">{{$noleaveapplications}}</div>
+            @endif
+          <a class="circle-tile-footer" href="{{ route('leaveapplication')}}">More Info<i class="fa fa-chevron-circle-right"></i></a>
+        </div>
+      </div>
+    </div> 
+     
+    <div class="col-lg-4 col-sm-8">
+      <div class="circle-tile ">
+        <a href="#"><div class="circle-tile-heading blue "><i class="fa fa-users fa-fw fa-3x"></i></div></a>
+        <div class="circle-tile-content blue">
+          <div class="circle-tile-description text-faded"> Permanent </div>
+          <div class="circle-tile-number text-faded ">10</div>
+          <a class="circle-tile-footer" href="#">More Info<i class="fa fa-chevron-circle-right"></i></a>
+        </div>
+      </div>
+    </div> 
+</div>
+</div>   
 
-                    <div class="col-md-6 col-lg-4 col-xlg-3">
-                        <div class="card card-info card-inverse">
-                            <div class="box text-center">
-                                @if($noleaveapplications=App\Models\LeaveApplication::count())
-                                <h1 class="font-light text-white">
-                                    {{$noleaveapplications}}
-                                </h1>
-                                @endif
-                                <h6 class="text-white">Leave Application</h6>
-                            </div>
-                        </div>
-                    </div>
-                    
-                    <!-- Column -->
-                    <div class="col-md-6 col-lg-4 col-xlg-3">
-                        <div class="card card-inverse card-danger">
-                            <div class="box text-center">
-                                <h1 class="font-light text-white">
-                                </h1>
-                                <h6 class="text-white">Attendances</h6>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div> 
+<div class="container bootstrap snippets bootdey">
+  <div class="row">
+    <div class="col-lg-4 col-sm-8">
+      <div class="circle-tile ">
+        <a href="#"><div class="circle-tile-heading green"><i class="fa fa-users fa-fw fa-3x"></i></div></a>
+        <div class="circle-tile-content green">
+          <div class="circle-tile-description text-faded"> Casual </div>
+            @if($noemployees=App\Models\Employee::count())
+                <div class="circle-tile-number text-faded ">{{$noemployees}}</div> 
+            @endif
+          <a class="circle-tile-footer" href="{{ route('employee.employees')}}">More Info<i class="fa fa-chevron-circle-right"></i></a>
+        </div>
+      </div>
+    </div>
+
+    <div class="col-lg-4 col-sm-8">
+      <div class="circle-tile ">
+        <a href="#"><div class="circle-tile-heading gray"><i class="fa fa-users fa-fw fa-3x"></i></div></a>
+        <div class="circle-tile-content gray">
+          <div class="circle-tile-description text-faded"> Job Order </div>
+            @if($noleaveapplications=App\Models\Leaveapplication::count())
+                <div class="circle-tile-number text-faded ">{{$noleaveapplications}}</div>
+            @endif
+          <a class="circle-tile-footer" href="{{ route('leaveapplication')}}">More Info<i class="fa fa-chevron-circle-right"></i></a>
+        </div>
+      </div>
+    </div> 
+     
+    <div class="col-lg-4 col-sm-8">
+      <div class="circle-tile ">
+        <a href="#"><div class="circle-tile-heading purple "><i class="fa fa-users fa-fw fa-3x"></i></div></a>
+        <div class="circle-tile-content purple">
+          <div class="circle-tile-description text-faded"> Co-Terminous </div>
+          <div class="circle-tile-number text-faded ">10</div>
+          <a class="circle-tile-footer" href="#">More Info<i class="fa fa-chevron-circle-right"></i></a>
+        </div>
+      </div>
+    </div> 
+</div>
+</div> 
+
+<style type="text/css">
+    .circle-tile {
+    margin-bottom: 15px;
+    text-align: center;
+}
+.circle-tile-heading {
+    border: 3px solid rgba(255, 255, 255, 0.3);
+    border-radius: 100%;
+    color: #FFFFFF;
+    height: 80px;
+    margin: 0 auto -40px;
+    position: relative;
+    transition: all 0.3s ease-in-out 0s;
+    width: 80px;
+}
+.circle-tile-heading .fa {
+    line-height: 80px;
+}
+.circle-tile-content {
+    padding-top: 50px;
+}
+.circle-tile-number {
+    font-size: 26px;
+    font-weight: 700;
+    line-height: 1;
+    padding: 5px 0 15px;
+}
+.circle-tile-description {
+    text-transform: uppercase;
+}
+.circle-tile-footer {
+    background-color: rgba(0, 0, 0, 0.1);
+    color: rgba(255, 255, 255, 0.5);
+    display: block;
+    padding: 5px;
+    transition: all 0.3s ease-in-out 0s;
+}
+.circle-tile-footer:hover {
+    background-color: rgba(0, 0, 0, 0.2);
+    color: rgba(255, 255, 255, 0.5);
+    text-decoration: none;
+}
+.circle-tile-heading.dark-blue:hover {
+    background-color: #2E4154;
+}
+.circle-tile-heading.green:hover {
+    background-color: #138F77;
+}
+.circle-tile-heading.orange:hover {
+    background-color: #DA8C10;
+}
+.circle-tile-heading.blue:hover {
+    background-color: #2473A6;
+}
+.circle-tile-heading.red:hover {
+    background-color: #CF4435;
+}
+.circle-tile-heading.purple:hover {
+    background-color: #7F3D9B;
+}
+.tile-img {
+    text-shadow: 2px 2px 3px rgba(0, 0, 0, 0.9);
+}
+
+.dark-blue {
+    background-color: #34495E;
+}
+.green {
+    background-color: #16A085;
+}
+.blue {
+    background-color: #2980B9;
+}
+.orange {
+    background-color: #F39C12;
+}
+.red {
+    background-color: #E74C3C;
+}
+.purple {
+    background-color: #8E44AD;
+}
+.dark-gray {
+    background-color: #7F8C8D;
+}
+.gray {
+    background-color: #95A5A6;
+}
+.light-gray {
+    background-color: #BDC3C7;
+}
+.yellow {
+    background-color: #F1C40F;
+}
+.text-dark-blue {
+    color: #34495E;
+}
+.text-green {
+    color: #16A085;
+}
+.text-blue {
+    color: #2980B9;
+}
+.text-orange {
+    color: #F39C12;
+}
+.text-red {
+    color: #E74C3C;
+}
+.text-purple {
+    color: #8E44AD;
+}
+.text-faded {
+    color: rgba(255, 255, 255, 0.7);
+}
+
+</style>
 
 @endsection
