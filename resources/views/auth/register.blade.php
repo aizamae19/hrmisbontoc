d0ca51fcec432f29d428ba32fbae1ff767f44354
aizamae19 <genolaiza@gmail.com>
2023-12-14 22:30:43 +0700

    modified add user

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,68 +1,215 @@
-@extends('layouts.auth-master')
+@extends('layouts.default')
 
 @section('content')
-    <div class="page-wrapper bg-gra-02 p-t-100 p-b-100 font-poppins">
-        <div class="wrapper wrapper--w680">
-            <div class="card card-4">
-                <div class="card-body">
-                    <h2 class="title">Add User</h2>
-                    <form method="POST" action="{{ route('register.custom') }}">
-                        @csrf
-                        @include('layouts.partials.messages')
-                        <div class="row row-space">
-                            <div class="col-2">
-                                <div class="input-group">
-                                    <label class="label">Name</label>
-                                    <input class="input--style-4" type="text" name="name">
-                                </div>
-                            </div>
-                            <div class="col-2">
-                                <div class="input-group">
-                                    <label class="label">Email</label>
-                                    <input class="input--style-4" type="text" name="email"placeholder = "example@gmail.com">
-                                </div>
-                            </div>
-                        </div>
-                        <div class="row row-space">
-                            <div class="col-2">
-                                <div class="input-group">
-                                    <label class="label">Username</label>
-                                    <input class="input--style-4" type="text" name="username">
-                                </div>
-                            </div>
-                            <div class="col-2">
-                                <div class="input-group" style="position: relative;">
-                                    <label class="label">Password</label>
-                                    <span class="toggle-password" onclick="togglePasswordVisibility()"
-                                          style="position: absolute; right: 8px; top: 70%; transform: translateY(-50%);">
-                                        <i class="fa fa-fw fa-eye field-icon" id="toggleIcon"></i>
-                                    </span>
-                                    <input class="input--style-4" type="password" name="password" id="password-field" >
-                                </div>
-                            </div>
-                        </div>
-                        <div class="row row-space">
-                            <div class="col-2">
-                                <div class="input-group">
-                                    <label class="label">Select Category</label>
-                                    <select name="type" class="input--style-4" class="user-select" style="padding: 10px 240px; border: 1px solid #ccc; border-radius: 4px;">
-                                        <option style="font-style: italic; color: #999;">
-                                        <option value="2">User</option>
-                                        <option value="1">Admin</option>
-                                    </select>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="p-t-15">
-                            <button class="btn btn--radius-2 btn--blue" type="submit">Submit</button>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div>
 
-    <script type="text/javascript">
+<style type="text/css">
+    /* all */
+@import url("https://fonts.googleapis.com/css2?family=Poppins:wght@400;500&display=swap");
+
+* {
+  margin-top: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: "Poppins", sans-serif;
+}
+
+:root {
+  --main-blue: #71b7e6;
+  --main-purple: #9b59b6;
+  --main-green: #014421;
+  --main-grey: #ccc;
+  --sub-grey: #d9d9d9;
+}
+
+/* container and form */
+.container {
+  max-width: 700px;
+  width: 100%;
+  background: #fff;
+  padding: 25px 30px;
+  border-radius: 5px;
+}
+.container .title {
+  font-size: 25px;
+  font-weight: 500;
+  position: relative;
+}
+
+.container .title::before {
+  content: "";
+  position: absolute;
+  height: 3.5px;
+  width: 30px;
+  background: linear-gradient(135deg, var(--main-blue), var(--main-green));
+  left: 0;
+  bottom: 0;
+}
+
+.container form .user__details {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  margin: 20px 0 12px 0;
+}
+/* inside the form user details */
+form .user__details .input__box {
+  width: calc(100% / 2 - 20px);
+  margin-bottom: 15px;
+}
+
+.user__details .input__box .details {
+  font-weight: 500;
+  margin-bottom: 5px;
+  display: block;
+}
+.user__details .input__box input {
+  height: 45px;
+  width: 100%;
+  outline: none;
+  border-radius: 5px;
+  border: 1px solid var(--main-grey);
+  padding-left: 15px;
+  font-size: 16px;
+  border-bottom-width: 2px;
+  transition: all 0.3s ease;
+}
+
+.user__details .input__box input:focus,
+.user__details .input__box input:valid {
+  border-color: var(--main-purple);
+}
+
+
+/* submit button */
+form .button {
+  height: 45px;
+  margin: 45px 0;
+}
+
+form .button input {
+  height: 100%;
+  width: 100%;
+  outline: none;
+  color: #fff;
+  border: none;
+  font-size: 18px;
+  font-weight: 500;
+  border-radius: 5px;
+  background: linear-gradient(135deg, var(--main-blue), var(--main-purple));
+  transition: all 0.3s ease;
+}
+
+form .button input:hover {
+  background: linear-gradient(-135deg, var(--main-blue), var(--main-purple));
+}
+
+select {
+   -webkit-appearance:none;
+   -moz-appearance:none;
+   -ms-appearance:none;
+   appearance:none;
+   outline:0;
+   box-shadow:none;
+   border:0!important;
+   background: #5c6664;
+   background-image: none;
+   flex: 1;
+   padding: 0 .5em;
+   color:#fff;
+   cursor:pointer;
+   font-size: 1em;
+   font-family: 'Open Sans', sans-serif;
+}
+select::-ms-expand {
+   display: none;
+}
+.select {
+   position: relative;
+   display: flex;
+   width: 40em;
+   height: 3em;
+   line-height: 3;
+   background: #ffffff;
+   overflow: hidden;
+   border-radius: .25em;
+}
+.select::after {
+   content: '\25BC';
+   position: absolute;
+   top: 0;
+   right: 0;
+   padding: 0 1em;
+   background: #2b2e2e;
+   cursor:pointer;
+   pointer-events:none;
+   transition:.25s all ease;
+}
+.select:hover::after {
+   color: #23b499;
+}
+
+@media only screen and (max-width: 584px) {
+  .container {
+    max-width: 100%;
+  }
+
+  form .user__details .input__box {
+    margin-bottom: 15px;
+    width: 100%;
+  }
+
+  form .gender__details .category {
+    width: 100%;
+  }
+
+  .container form .user__details {
+    max-height: 300px;
+    overflow-y: scroll;
+  }
+
+  .user__details::-webkit-scrollbar {
+    width: 0;
+  }
+}
+
+</style>
+
+<div class="container">
+  <div class="title">Register New User</div>
+  <form method="POST" action="{{ route('register.custom') }}">
+    @csrf
+    <div class="user__details">
+      <div class="input__box">
+        <span class="details">Full Name</span>
+        <input type="text" name="name" required>
+      </div>
+      <div class="input__box">
+        <span class="details">Username</span>
+        <input type="text" name="username" required>
+      </div>
+      <div class="input__box">
+        <span class="details">Email</span>
+        <input type="email" name="email" placeholder="example@gmail.com" required>
+      </div>
+      <div class="input__box">
+        <span class="details">Password</span>
+        <input type="password" name="password" id="password-field" required>
+      </div>
+    </div>
+    <div class="select">
+   <select name="type" id="format">
+      <option selected disabled>Select Role</option>
+      <option value="2">User</option>
+      <option value="1">Admin</option>
+   </select>
+</div>
+    <div class="button">
+      <input type="submit" value="Register">
+    </div>
+  </form>
+</div>
+
+<script type="text/javascript">
         function togglePasswordVisibility() {
     const passwordField = document.getElementById('password-field');
     const toggleIcon = document.getElementById('toggleIcon');
